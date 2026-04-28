acfcb768a72894c9b9d4d1990bad818d7c0ed9ba
AG3s-Studio <altforofferupanddiscord@gmail.com>
2026-04-28 10:58:32 -0700

    Update guesser.js
    added a secret text thing for fun and for my friends

--- guesser.js
+++ guesser.js
@@ -1,11 +1,12 @@
-let number2guess = Math.floor(Math.random()*15) + 1;
+let number2guess = Math.floor(Math.random()*100) + 1;
 let attempts = 0;
 
 const input = document.getElementById("guessInput");
 const button = document.getElementById("guessBtn");
+const secrettext = document.getElementById("secretMessage");
 let message = document.getElementById("result");
 const attemptsText = document.getElementById("attempts");
-
+secrettext.textContent = btoa("My Name Is Anderdingus")
 button.addEventListener("click", function () {
   let guess = Number(input.value);
   attempts++;
@@ -20,4 +21,3 @@ button.addEventListener("click", function () {
   
   attemptsText.textContent = `Attempts: ${attempts}`;
 });
-
