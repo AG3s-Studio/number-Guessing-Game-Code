b2e301897e519cba994be348e29f96f97d9eb5a4
AG3s-Studio <altforofferupanddiscord@gmail.com>
2026-04-29 10:55:33 -0700

    Update guesser.js
    Added a guess my name thing

--- guesser.js
+++ guesser.js
@@ -1,14 +1,28 @@
 let number2guess = Math.floor(Math.random()*100) + 1;
 let attempts = 0;
+let attempts2 = 0;
+const ginput = document.getElementById("guessInput");
 
-const input = document.getElementById("guessInput");
-const button = document.getElementById("guessBtn");
+const gbutton = document.getElementById("guessBtn");
+
+const ninput = document.getElementById("myName");
+
+const nbutton = document.getElementById("GuessName")
+
 const secrettext = document.getElementById("secretMessage");
+
 let message = document.getElementById("result");
-const attemptsText = document.getElementById("attempts");
-secrettext.textContent = btoa("My Name Is Anderdingus")
-button.addEventListener("click", function () {
-  let guess = Number(input.value);
+
+let attemptsText = document.getElementById("attempts");
+
+let message2 = document.getElementById("result2");
+
+let attemptsText2 = document.getElementById("attempts2");
+
+secrettext.textContent = btoa("My Name Is Anderdingus");
+
+gbutton.addEventListener("click", function () {
+  let guess = Number(ginput.value);
   attempts++;
   
   if (guess === number2guess) {
@@ -20,4 +34,16 @@ button.addEventListener("click", function () {
   }
   
   attemptsText.textContent = `Attempts: ${attempts}`;
+});
+
+nbutton.addEventListener("click", function () {
+  let g2 = ninput.value
+  if (g2 === "Anthony Glenn III") {
+    message2.textContent = "Congrats!"
+  } else {
+    message2.textContent = "Try Again!"
+  }
+  
+  attempts2++;
+  attemptsText2.textContent = `Attempts: ${attempts2}`
 });
